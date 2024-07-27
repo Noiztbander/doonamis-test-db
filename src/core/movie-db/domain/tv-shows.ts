b7e381adb1ca443b9aee983581638db13db3a419
Erick Noiztbander <aventum2@gmail.com>
2024-07-27 03:57:24 +0200

    chore: add tv shows detail type

--- src/core/movie-db/domain/tv-shows.ts
+++ src/core/movie-db/domain/tv-shows.ts
@@ -1,5 +1,12 @@
 import { Keyable } from "@/core/types/Keyable";
 
+export interface ITvShowEntity extends Keyable {
+  page: number;
+  results: ITvShow[];
+  total_pages: number;
+  total_results: number;
+}
+
 export interface ITvShow {
   adult?: boolean;
   backdrop_path?: string | null;
@@ -17,9 +24,76 @@ export interface ITvShow {
   vote_count?: number;
 }
 
-export interface ITvShowEntity extends Keyable {
-  page: number;
-  results: ITvShow[];
-  total_pages: number;
-  total_results: number;
+export interface ITvShowDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string | null;
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    air_date: string;
+    episode_number: number;
+    episode_type: string;
+    production_code: string;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path: string | null;
+  };
+  name: string;
+  next_episode_to_air: string | null;
+  networks: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string | null;
+    season_number: number;
+    vote_average: number;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
 }
