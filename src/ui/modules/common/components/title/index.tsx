7d54e65ab848367f745d724ba1390e00b37d8f44
Erick Noiztbander <aventum2@gmail.com>
2024-07-29 01:28:19 +0200

    chore: add title component

---
+++ src/ui/modules/common/components/title/index.tsx
@@ -0,0 +1,19 @@
+import { Component, ReactNode } from "react";
+
+import "./title.css";
+
+interface IBasicTitleProps {
+  title: string;
+  icon: ReactNode;
+}
+
+export default class BasicTitle extends Component<IBasicTitleProps> {
+  render(): ReactNode {
+    return (
+      <div className="basicTitle_container">
+        <h1>{this.props.title}</h1>
+        {this.props.icon && this.props.icon}
+      </div>
+    );
+  }
+}
