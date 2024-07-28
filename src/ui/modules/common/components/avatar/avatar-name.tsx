ff7b0217f618d8a0361b43aa634bc7d04f950fa7
Erick Noiztbander <aventum2@gmail.com>
2024-07-28 22:07:39 +0200

    chore: add avatar component

---
+++ src/ui/modules/common/components/avatar/avatar-name.tsx
@@ -0,0 +1,31 @@
+import { Component, ReactNode } from "react";
+import Image from "next/image";
+
+import "./avatar-name.css";
+
+interface IAvatarProps {
+  name: string;
+  src?: string;
+}
+
+export default class AvatarName extends Component<IAvatarProps> {
+  render(): ReactNode {
+    return (
+      <div className="avatarName_container">
+        {this.props.src && (
+          <div className="image_container">
+            <Image
+              width={600}
+              height={900}
+              src={this.props.src}
+              alt={this.props.name}
+              loading="lazy"
+            />
+          </div>
+        )}
+
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
